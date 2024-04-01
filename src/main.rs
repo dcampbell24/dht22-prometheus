6a5f95b67da54e9788ef5c0f0a9f80efeac5af5b
David Campbell <dcampbell24@gmail.com>
2024-04-01 17:39:04 -0400

    Test dht22.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{net::{Ipv4Addr, SocketAddrV4}, thread::sleep, time::Duration};
 use dht_embedded::{Dht22, DhtSensor, NoopInterruptControl};
 use gpio_cdev::{Chip, LineRequestFlags};
 use linux_embedded_hal::{CdevPin, Delay};
-use metrics::gauge;
+// use metrics::gauge;
 use metrics_exporter_prometheus::PrometheusBuilder;
 
 fn main() {
@@ -13,13 +13,13 @@ fn main() {
         .install()
         .expect("failed to install recorder/exporter");
 
-    let humidity = gauge!("humidity_percent_2");
-    let temperature_c = gauge!("temperature_celsius_2");
-    let temperature_f = gauge!("temperature_fahrenheit_2");
+    // let humidity = gauge!("humidity_percent_2");
+    // let temperature_c = gauge!("temperature_celsius_2");
+    // let temperature_f = gauge!("temperature_fahrenheit_2");
 
     // TODO: figure out the right device.
     let mut gpiochip = Chip::new("/dev/gpiochip0").unwrap();
-    let line = gpiochip.get_line(24).unwrap();
+    let line = gpiochip.get_line(23).unwrap();
     let handle = line
         .request(
             LineRequestFlags::OPEN_DRAIN | LineRequestFlags::INPUT | LineRequestFlags::OUTPUT,
@@ -33,19 +33,18 @@ fn main() {
     loop {
         match sensor.read() {
             Ok(reading) => {
-                /*
-                print!("{}{}{}", termion::clear::All, termion::cursor::Goto(0, 0),
+
+                print!("{}{}", termion::clear::All, termion::cursor::Goto(0, 0));
                 println!(
                     "{}°C {}°F {}% RH",
                     reading.temperature(),
                     (reading.temperature() * 1.8) + 32.0,
                     reading.humidity()
                 );
-                */
 
-                humidity.set(reading.humidity());
-                temperature_c.set(reading.temperature());
-                temperature_f.set((reading.temperature() * 1.8) + 32.0);
+                // humidity.set(reading.humidity());
+                // temperature_c.set(reading.temperature());
+                // temperature_f.set((reading.temperature() * 1.8) + 32.0);
 
                 sleep(Duration::from_millis(2_100));
             }
